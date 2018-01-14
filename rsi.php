3b51b41babee2e90dc23118aab3c1ecb8f393d6f
cihangirerdem <chngrerdem@gmail.com>
2018-01-14 17:53:48 +0300

    Moving average gerekliliğinden ara check in

--- rsi.php
+++ rsi.php
@@ -24,32 +24,60 @@ $prevValue = 0;
 $currentValue = 0;
 $value = 0;
 
-while(true) {
-	$ticks = $api->candlesticks($key, "1m");
-	end($ticks);
-	$tick =  prev($ticks);
-	$currentTimeInstant = intval($tick["openTime"]);
-	if ($currentTimeInstant != $prevTimeInstant) {
-		foreach($keys as $key) {
-			if($key != 'ETHBTC') {
-				continue;
-			}
-			
-				$currentValue = $tick["high"];
-				$value = $currentValue - $prevValue;
+// Get Last 500 point
+	$ticks = $api->candlesticks("ETHBTC", "1m");
+	$ptick = current($ticks);
+	$ctick = next($ticks);
+	$currentTimeInstant = $ctick["openTime"];
+	$periodNum = 1;
+	$averageGain = 0;
+	$averageLoss = 0;
+	$RS = 0;
+	$RSI = 0;
+for($x=0; $x<=498; $x++){
+			$value = $ctick["high"] - $ptick["high"];
+			echo $key."\t".$x."\t".$ptick["high"]."\t".$ctick["high"]."\t".$value."\t".$ctick["volume"]."\t".PHP_EOL;
+			$ptick = current($ticks);
+			$ctick = next($ticks);
+			if($periodNum <= 14){
 				if($value>=0)
-					$cumSumPositiveValues = $cumSumPositiveValues + $value;
+					$cumSumPositiveValues += $value;
 				else
-					$cumSumNegativeValues = $cumSumNegativeValues + $value;
+					$cumSumNegativeValues += $value;
 				
-				echo $key."\t".$tick["high"]."\t".$value."\t".$tick["volume"]."\t".$tick["openTime"].PHP_EOL;
-				$prevTimeInstant = $currentTimeInstant;
-				$prevValue = $currentValue;
+				$periodNum += 1;
+			}
+			else
+			{
+				$periodNum = 1;
+				$averageGain = $cumSumPositiveValues /14.0;
+				$averageLoss = abs($cumSumNegativeValues)/14.0;
+				$RS = averageGain / averageLoss;
+				$RSI = 100 - (100/(1+RS));
+					
+			}
 				
+				
+				
+			
 		}
+
+		$prevTimeInstant = $currentTimeInstant;
+		
+		
+		// Continue to calculate RSI from newer values
+while(true) {
+	$ticks = $api->candlesticks("ETHBTC", "1m");
+	// Get last candlesticks
+	end($ticks);
+	$ctick = prev($ticks);
+	$currentTimeInstant = $ctick["openTime"];
+	
+	if ($currentTimeInstant != $prevTimeInstant) {
+		
+		echo $key."\t".$currentTimeInstant.PHP_EOL;
+		
+			$prevTimeInstant = $currentTimeInstant;
 	}
-    
 }
-
-
 ?>
